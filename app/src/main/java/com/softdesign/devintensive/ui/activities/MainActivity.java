bb7323c32477de10121279202ca028a4e6fec84b
Alex <kastiel1995@gmail.com>
2016-06-30 22:17:58 +0300

    fixed file

--- app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
@@ -1,63 +1,261 @@
 package com.softdesign.devintensive.ui.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.softdesign.devintensive.R;
+import com.softdesign.devintensive.data.managers.DataManager;
 import com.softdesign.devintensive.utils.ConstantManager;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
-    private static final String TAG = ConstantManager.TAG_PREFIX+"Main Activity";
+public class MainActivity extends BaseActivity implements View.OnClickListener {
+
+    private static final String TAG = ConstantManager.TAG_PREFIX + "Main Activity";
+
+    private DataManager mDataManager;
+
+    private int mCurrentEditMode = 0;
+
+    private ImageView mCallImg;
+    private CoordinatorLayout mCoordinatorLayout;
+    private Toolbar mToolbar;
+    private DrawerLayout mNavigationDrawer;
+    private FloatingActionButton mFab;
+    private EditText mUserPhone, mUserMail, mUserVk, mUserGit, mUserBio;
+    private List<EditText> mUserInfoViews;
 
     /**
+     * Метод вызывается при создании активити после изменения конфигурации/возврата к текушей
+     * активити после его уничтожения.
      *
-     * @param savedInstanceState
+     * в данном методе инициализируются/производится:
+     * - UI пользовательский интерфейс (статика)
+     * - инициализация статических данных активити
+     * - связь данных со списками (инициализация адаптеров)
+     *
+     * Не запускать длительные операции по работе с данными в onCreate() !!!
+     *
+     * @param savedInstanceState - объект со значениями сохраненными в Bundle - состояние UI
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG,"onCreate");
+
+        Log.d(TAG, "onCreate");
+
+        mDataManager = DataManager.getInstance();
+        mCallImg = (ImageView) findViewById(R.id.call_img);
+        mCoordinatorLayout = (CoordinatorLayout) findViewById(R.id.main_coordinator_conteiner);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mNavigationDrawer = (DrawerLayout) findViewById(R.id.navigation_drawer);
+        mFab = (FloatingActionButton) findViewById(R.id.fab);
+        mUserPhone = (EditText) findViewById(R.id.editPhone);
+        mUserMail = (EditText) findViewById(R.id.editEmail);
+        mUserVk = (EditText) findViewById(R.id.editVk);
+        mUserGit = (EditText) findViewById(R.id.editGit);
+        mUserBio = (EditText) findViewById(R.id.editProfile);
+
+        mUserInfoViews = new ArrayList<>();
+        mUserInfoViews.add(mUserPhone);
+        mUserInfoViews.add(mUserMail);
+        mUserInfoViews.add(mUserVk);
+        mUserInfoViews.add(mUserGit);
+        mUserInfoViews.add(mUserBio);
+
+        mFab.setOnClickListener(this);
+
+        setupToolbar();
+        setupDrawer();
+        loadUserInfoValue();
+
+        if (savedInstanceState == null){
+            //активити запускается впервые
+
+        } else {
+            //активити уже создавалось
+            mCurrentEditMode = savedInstanceState.getInt(ConstantManager.EDIT_MODE_KEY, 0);
+            changeEditMode(mCurrentEditMode);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home){
+            mNavigationDrawer.openDrawer(GravityCompat.START);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     /**
-     *
+     * Метод вызывается при старте активити перед моментом того как UI станет достепен пользователю
+     * как правило в данном методе происходит регистрация подписки на событиея остановка которых была
+     * произведена в onStop()
      */
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d(TAG,"onStart");
+        Log.d(TAG, "onStart");
     }
 
+    /**
+     * Метод вызывается когда активити становится доступен пользователю для взаимодействия
+     * в данном методе как правило происходит запуск анимаций/аудио/видео/запуск BroadcastReceiver
+     * необходимых для реализации UI логика/запуск выполнение потоков и т.д.
+     * метод должен быть максимально легковесным для максимальной отзывчивости UI
+     */
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d(TAG,"onResume");
+        Log.d(TAG, "onResume");
     }
 
-
+    /**
+     * Метод вызывается когда текущая активити теряет фокус но остается видимой(всплытие
+     * диалогового окна/частичное перекрытие другой активити и т.д)
+     * <p/>
+     * в данном методе реализуют сохранение легковесных UI данных/анимаций/аудио/видео  и т.д
+     */
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d(TAG,"onPause");
+        Log.d(TAG, "onPause");
+        saveUserInfoValue();
     }
 
+    /**
+     * Метод вызывается когда активити становится невидимым для пользователя.
+     * в данном методе происходит отписка от событий, остановка сложных анимаций, сложные операции по
+     * сохранению данных/прерывание запущенных потоков и т.д.
+     */
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d(TAG,"onStop");
+        Log.d(TAG, "onStop");
     }
+
+    /**
+     * Метод вызывается при окончании работы активити (когда это происходит системно или после вызова
+     * метода finish())
+     */
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d(TAG,"onDestroy");
+        Log.d(TAG, "onDestroy");
     }
 
+    /**
+     * Метод вызывается при рестарте активити/возобнавлении работы после вызова метода onStop()
+     * в данном методе реализуется спецификация бизнес логики которая должна быть реализована именно
+     * при рестарте активности - например запрос к серверу который необходимо вызвать при возвращении
+     * из другой активности (обновление данных, подписка на определенное событие проинициализированное
+     * на другом экране/специфическая бизнес логика завязанная именно на перезапуске активити)
+     */
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.d(TAG,"onRestart");
+        Log.d(TAG, "onRestart");
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.fab:
+                if (mCurrentEditMode == 0){
+                    changeEditMode(1);
+                    mCurrentEditMode = 1;
+                } else {
+                    changeEditMode(0);
+                    mCurrentEditMode = 0;
+                }
+                break;
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        outState.putInt(ConstantManager.EDIT_MODE_KEY, mCurrentEditMode);
+    }
+
+    private void showSnackbar(String message){
+        Snackbar.make(mCoordinatorLayout, message, Snackbar.LENGTH_LONG).show();
+    }
+
+    private void setupToolbar(){
+        setSupportActionBar(mToolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null){
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_dehaze_black_24dp);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    private void setupDrawer(){
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                showSnackbar(item.getTitle().toString());
+                item.setChecked(true);
+                mNavigationDrawer.closeDrawer(GravityCompat.START);
+                return false;
+            }
+        });
+    }
+
+    /**
+     * переключает режим редактирования
+     * @param mode если 1 режим редактирования, если 0 режим просмотра
+     */
+    private void changeEditMode(int mode){
+        if (mode == 1) {
+            mFab.setImageResource(R.drawable.ic_create_black_24dp);
+            for (EditText userValue : mUserInfoViews) {
+
+                userValue.setEnabled(true);
+                userValue.setFocusable(true);
+                userValue.setFocusableInTouchMode(true);
+            }
+        } else {
+            mFab.setImageResource(R.drawable.ic_done_black_24dp);
+            for (EditText userValue : mUserInfoViews) {
+
+                userValue.setEnabled(false);
+                userValue.setFocusable(false);
+                userValue.setFocusableInTouchMode(false);
+                saveUserInfoValue();
+            }
+        }
+    }
+
+    private void loadUserInfoValue(){
+        List<String> userData = mDataManager.getPreferencesManager().loadUserProfileData();
+        for (int i = 0; i < userData.size(); i++) {
+            mUserInfoViews.get(i).setText(userData.get(i));
+        }
+    }
+
+    private void saveUserInfoValue(){
+        List<String> userData = new ArrayList<>();
+        for (EditText userFieldView : mUserInfoViews) {
+            userData.add(userFieldView.getText().toString());
+        }
+        mDataManager.getPreferencesManager().saveUserProfileData(userData);
     }
 }
