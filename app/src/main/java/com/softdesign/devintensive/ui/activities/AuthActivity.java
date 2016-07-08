19f04bdeeb604c43229f9e52419fa30f30766e30
Alex <kastiel1995@gmail.com>
2016-07-08 23:55:15 +0300

    Hometask_4 with bad refactoring, i am sorry hard week

---
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/AuthActivity.java
@@ -0,0 +1,45 @@
+package com.softdesign.devintensive.ui.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.softdesign.devintensive.R;
+
+
+public class AuthActivity extends BaseActivity{
+
+    private EditText mLoginField;
+    private EditText mPassField;
+    private Button mButton;
+    private TextView mForgetPass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+
+        mLoginField = (EditText)findViewById(R.id.email_field);
+        mPassField = (EditText)findViewById(R.id.pass_field);
+        mButton = (Button)findViewById(R.id.enter_button);
+        mForgetPass = (TextView)findViewById(R.id.forget_pass);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startMainActivity();
+            }
+        });
+
+    }
+
+    private void startMainActivity(){
+        Intent intentActivity = new Intent(this,MainActivity.class);
+        startActivity(intentActivity);
+        finish();
+    }
+}
